990f53d3c2bd91ae9f746f8f934fa56dbcbf56d7
kravets@qarea.com <kravets@qarea.com>
2015-05-11 12:54:15 +0300

    Added comments on source codes; added link for Donwload Data button

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
+    // Custom messages to be added to info window. Country name should be in English and upper-cased
     var specialCountryMessages = {};
 
     specialCountryMessages["Namibia".toUpperCase()] = "Walvis Bay, Namibia is a major transit port. It is likely that timber shipped to Namibia is subsequently shipped on to another country.";
     specialCountryMessages["Republic of Congo".toUpperCase()] = "Pointe Noire, Rep. of Congo is a major transit port. It is likely that timber shipped to Republic of Congo is subsequently shipped on to another country.";
 
+    // Function to format floating-point number to use commas as thousands
+    // separator, point as decimal separator and keep only 2 decimal digits: 0,000,000.00
     var formatNumber = function(n) {
         var n = parseFloat('' + n);
         if (isNaN(n) || !isFinite(n)) return '0,00';
@@ -19,6 +22,8 @@ $(document).ready(function() {
         return x1 + x2;
     }
 
+    // Cross-browser way to trigger global resize event.
+    // Call this function to force map update
     forceResizeEvent = function() {
         var resizer = function() {
             if (window.fireEvent) {
@@ -36,6 +41,7 @@ $(document).ready(function() {
         setTimeout(resizer, 10);
     }
 
+    // Hashmap to cache any data
     var storage = {
         set: function(data, varName) {
             storage.data[varName] = data;
@@ -46,12 +52,14 @@ $(document).ready(function() {
         data: {}
     }
 
+    // Click handler for checkboxes in slider.
+    // Hint: there was a task to remove these checkboxes. They were just hidden,
+    // so they can be easily turned back
     $(".filter-type").click(function(event) {
-
         filterShipperList($(this).attr('id'), this);
-
     });
 
+    // Table sorting, using jQuery plugin
     $('.js-tablesort').stupidtable().on("beforetablesort", function (event, data) {
         $(this).find('.js-icon')
             .removeClass('glyphicon-triangle-top')
@@ -66,13 +74,10 @@ $(document).ready(function() {
     });
 
     //
-    //
-    //
-    // Generates time period
-    //
-    //
+    // Time filters
     //
 
+    // Initialize dropdown calendars (jQuery plugin)
     $('.date-filter input').pickmeup({
         position: 'bottom',
         format: 'Y-m-d',
@@ -81,6 +86,7 @@ $(document).ready(function() {
             $(this).change();
         }
     });
+    // Handle calendar change event
     $('.date-filter input').on('change', function(event){
         var input = $(this);
         input.parent().find('span').text(input.val());
@@ -100,7 +106,7 @@ $(document).ready(function() {
             getTopExportingDestinations(storage.get('dateArray'));
         }
     });
-
+    // Initialize filter with actual min and max date from database
     (function(){
         $.get('http://datahub.io/api/action/datastore_search_sql?sql=SELECT MIN(SUBSTRING(departure_date FROM 1 FOR 10)) AS min, MAX(SUBSTRING(departure_date FROM 1 FOR 10)) AS max FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7"', function(data) {
             var range = data.result.records[0];
@@ -119,14 +125,10 @@ $(document).ready(function() {
     })();
 
     //
+    // Initialize common data and tables. Data is loaded for full available range
     //
-    //
+
     // Get base map data
-    //
-    //
-    //
-
-
     function getBaseData(dateArray) {
         $.get('http://datahub.io/api/action/datastore_search_sql?sql=SELECT "destination_country","lat","lon",sum("weight_rwe"),"shipper_description","shipper_type","species" FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7" WHERE "departure_date" BETWEEN \'' + dateArray[0] + '\' AND \'' + dateArray[1] + '\' GROUP BY "destination_country","lat","lon","shipper_description","shipper_type","species"', function(data) {
             // Data raw
@@ -135,16 +137,7 @@ $(document).ready(function() {
         });
     }
 
-
-    //
-    //
-    //
     // getTopExportingCompanies
-    //
-    //
-    //
-
-
     function getTopExportingCompanies(dateArray) {
         $.get('http://datahub.io/api/action/datastore_search_sql?sql=SELECT "shipper",sum("weight_rwe") FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7" WHERE "departure_date" BETWEEN \'' + dateArray[0] + '\' AND \'' + dateArray[1] + '\' GROUP BY "shipper" ORDER BY sum("weight_rwe") DESC', function(data) {
             $("#topExportingCompanies .js-icon")
@@ -162,15 +155,7 @@ $(document).ready(function() {
         });
     }
 
-
-    //
-    //
-    //
     // getTopExportingDestinations
-    //
-    //
-    //
-
     function getTopExportingDestinations(dateArray) {
         $.get('http://datahub.io/api/action/datastore_search_sql?sql=SELECT "destination_country",sum("weight_rwe") FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7" WHERE "departure_date" BETWEEN \'' + dateArray[0] + '\' AND \'' + dateArray[1] + '\' GROUP BY "destination_country" ORDER BY sum("weight_rwe") DESC', function(data) {
             $("#topExportingDestinations .js-icon")
@@ -188,16 +173,7 @@ $(document).ready(function() {
         });
     }
 
-
-    //
-    //
-    //
     // Get exported species when dropdown was changed, will change when select route.
-    //
-    //
-    //
-
-
     function getTopExportingSpecies(dateArray) {
         $("#speciesTableHeading").html('Species exported');
         $.fn.matchHeight._update();
@@ -218,14 +194,7 @@ $(document).ready(function() {
     }
 
 
-    //
-    //
-    //
-    // General init funtion
-    //
-    //
-    //
-
+    // General initialization function
     function initData(data) {
         var dataLocations = [];
         var dataDestirnations = [];
@@ -241,7 +210,7 @@ $(document).ready(function() {
         }, collected);
         var DRCData = _.values(collected);
 
-        // Route and markers quantity loop ( 20 is ok )
+        // Route and markers quantity loop
         // Max. 30 routes
         var n = DRCData.length > 31 ? 31 : DRCData.length;
         for (var i = 1; i < n; i++) {
@@ -258,17 +227,8 @@ $(document).ready(function() {
     }
 
 
-    //
-    //
-    //
     // Generates data for markers
-    //
-    //
-    //
-
     function generateDestirnation(data) {
-
-
         var dataDestirnationObj = {}
 
         if (data == 'default') {
@@ -285,19 +245,10 @@ $(document).ready(function() {
             dataDestirnationObj.color = '#dd202c';
         }
         return dataDestirnationObj
-
     }
 
-    //
-    //
-    //
     // Generates data for routes
-    //
-    //
-    //
-
     function generateLocation(data) {
-
         var dataLocation = {};
         dataLocation.country = data.destination_country;
         dataLocation.weight = data.sum;
@@ -305,24 +256,60 @@ $(document).ready(function() {
             new google.maps.LatLng(-3.693714, 23.991271), // Republic of Congo
             new google.maps.LatLng(data.lat, data.lon)
 
-        ]
-
-        return dataLocation
-
+        ];
+        return dataLocation;
     }
 
+    //
+    // Slider related functions
+    //
+    $('input[type=checkbox]').change(function() {
+        var filterArray = []
+        if ($('#concession').prop("checked")) {
+            filterArray.push("concession");
+        }
+        if ($('#artisinal').prop("checked")) {
+            filterArray.push("artisinal");
+        }
+        filterSlider(filterArray);
+    });
 
-    //
-    //
-    //
+    function initSlider() {
+        var markdown = new Showdown.converter();
+        var slider = $("#slider");
+        var data = storage.get("dataShippersCountry");
+        slider.empty();
+        for (var i = 0; i < data.length; i++) {
+            slider.append('<div class="slider-item"><div class="weight pull-left text-nowrap">' +
+            formatNumber(data[i].sum) + ' tonnes</div><div class="inner-content"><h3>' + data[i].shipper +
+            '</h3><div class="slider-desc">' + markdown.makeHtml(data[i].shipper_description) + '</div>' +
+            '<span class="js-expand text-center slider-desc-shadow"><i class="glyphicon glyphicon-chevron-down"></i></span>' +
+            '</div></div>');
+        };
+        $('#slider').parent().scrollTop(0);
+    }
+
+    function filterSlider(filterArray) {
+        if (filterArray.length != 0) {
+            var data = storage.get("dataShippersCountry_bak");
+            var data_new = _.filter(data, function(shipper) {
+                return shipper.shipper_type.toLowerCase() == filterArray[0] || shipper.shipper_type.toLowerCase() == filterArray[1]
+            });
+            storage.set(data_new, "dataShippersCountry");
+        } else {
+            storage.set(storage.get('dataShippersCountry_bak'), "dataShippersCountry");
+        }
+        initSlider();
+    }
+
+    $('#slider').on('click', '.js-expand', function(event) {
+        $(this).parent().find('.slider-desc').toggleClass('expanded');
+        $(this).find('.glyphicon').toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up');
+    });
+
     // Initializes the map
-    //
-    //
-    //
-
-
+    // Hint: this function has a lot of utility functions inside of its' body
     function initializeMap(dataLocations, dataDestirnations) {
-
         var mapOptions = {
             // Shift map down because most markers are on the top of map
             center: new google.maps.LatLng(-3.693714 + 30, 23.991271),
@@ -333,7 +320,6 @@ $(document).ready(function() {
             mapTypeControl: false,
             streetViewControl: false
         };
-
 
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
@@ -361,7 +347,6 @@ $(document).ready(function() {
         };
 
         var markers = []
-
 
         // Set infowindows
         function setInfoWindows(country) {
@@ -400,7 +385,6 @@ $(document).ready(function() {
             $("#country").text(country);
         }
 
-
         function generateShipperList(country) {
             var dateArray = storage.get('dateArray');
             var URL = 'http://datahub.io/api/action/datastore_search_sql?sql=SELECT "shipper","destination_country","shipper_type",sum("weight_rwe"),"shipper_description" FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7" WHERE "departure_date" BETWEEN \'' + dateArray[0] + '\' AND \'' + dateArray[1] + '\'  AND "destination_country" = \'' + country + '\' GROUP BY "shipper","destination_country","shipper_type","shipper_description" ORDER by sum("weight_rwe") DESC'
@@ -413,7 +397,6 @@ $(document).ready(function() {
                 initSlider();
             });
 
-
             var URL = 'http://datahub.io/api/action/datastore_search_sql?sql=SELECT "destination_country",sum("weight_rwe") FROM "7c936579-7940-42a3-ae79-a0f498cb7ea7" WHERE "departure_date" BETWEEN \'' + dateArray[0] + '\' AND \'' + dateArray[1] + '\'  AND "destination_country" = \'' + country + '\' GROUP BY "destination_country"'
             $.get(URL, function(data) {
                 $("#countrySelectedAmount").text(formatNumber(data.result.records[0].sum) + " tonnes");
@@ -423,89 +406,11 @@ $(document).ready(function() {
             $.get(URL, function(data) {
                 $("#speciesNumber").text(data.result.records[0].count);
             });
-
-
-
         }
 
-   $('input[type=checkbox]').change(function() {
-        var filterArray = []
-        if ($('#concession').prop("checked")) {
-            filterArray.push("concession");
-        }
-        if ($('#artisinal').prop("checked")) {
-            filterArray.push("artisinal");
-        }
-        filterSlider(filterArray);
-    });
-
-
-
-    //
-    //
-    //
-    // Init slider
-    //
-    //
-    //
-
-
-    function initSlider() {
-        var markdown = new Showdown.converter();
-        var slider = $("#slider");
-        var data = storage.get("dataShippersCountry");
-        slider.empty();
-        for (var i = 0; i < data.length; i++) {
-            slider.append('<div class="slider-item"><div class="weight pull-left text-nowrap">' +
-                formatNumber(data[i].sum) + ' tonnes</div><div class="inner-content"><h3>' + data[i].shipper +
-                '</h3><div class="slider-desc">' + markdown.makeHtml(data[i].shipper_description) + '</div>' +
-                '<span class="js-expand text-center slider-desc-shadow"><i class="glyphicon glyphicon-chevron-down"></i></span>' +
-                '</div></div>');
-        };
-        $('#slider').parent().scrollTop(0);
-    }
-
-        $('#slider').on('click', '.js-expand', function(event) {
-            $(this).parent().find('.slider-desc').toggleClass('expanded');
-            $(this).find('.glyphicon').toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up');
-        });
-
-
-        
-    //
-    //
-    //
-    // FilterSlider
-    //
-    //
-    //
-
-
-
-    function filterSlider(filterArray) {
-        if (filterArray.length != 0) {
-            var data = storage.get("dataShippersCountry_bak");
-            var data_new = _.filter(data, function(shipper) {
-                return shipper.shipper_type.toLowerCase() == filterArray[0] || shipper.shipper_type.toLowerCase() == filterArray[1]
-            });
-            storage.set(data_new, "dataShippersCountry");
-        } else {
-            storage.set(storage.get('dataShippersCountry_bak'), "dataShippersCountry");
-        }
-        initSlider();
-    }
-
-        //
-        //
-        //
-        // Sets content for info windows
-        //
-        //
-        //
-
-
+        // Sets content for info window
         function setContent(country) {
-                        var totalWeight = 0;
+            var totalWeight = 0;
             var species = [];
 
             _.each(storage.get('baseData'), function(countryEach) {
@@ -577,7 +482,8 @@ $(document).ready(function() {
 
         }
 
-
+        // Event handler for Close button in slider.
+        // Clear marker and route selection, hide info window and slider
         $('.js-close-slider').on('click', function(event){
             $(".map-block").removeClass('slider-visible');
             var lastItem = storage.get("lastMarker");
@@ -590,15 +496,7 @@ $(document).ready(function() {
             forceResizeEvent();
         });
 
-
-        //
-        //
-        //
         // Generates markers
-        //
-        //
-        //
-
         var maxWeight = 0;
         _.each(dataDestirnations, function(item) {
             if (item.weight > maxWeight) {
@@ -648,22 +546,14 @@ $(document).ready(function() {
         });
 
 
-        //
-        //
-        //
         // Generates routes
-        //
-        //
-        //
         var lineSymbol = {
             path: 'M 0,-1 0,3',
             strokeOpacity: 1,
             scale: 3
         };
 
-
         _.each(dataLocations, function(item) {
-
             var route = new google.maps.Polyline({
                 geodesic: true,
                 country: item.country,
@@ -701,11 +591,6 @@ $(document).ready(function() {
             });
         });
 
-
-
     }
-
-
-
 
 });
